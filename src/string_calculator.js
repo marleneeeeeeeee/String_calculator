021dc2919ffbcac599802ccd0204a0f09c0afec4
marleneeeeeeeee <ma.haberhauer@gmail.com>
2026-05-06 11:15:05 +0200

    TC_4 implemented and green, Refactoring needed TC_1

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,5 +1,11 @@
 // StringCalculator.js
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
-	return string_numbers.length;
+	if (string_numbers.includes(',')) {
+		result = parseInt(string_numbers.split(',')[0]) + parseInt(string_numbers.split(',')[1]);
+	}
+	 else if (string_numbers.length > 0) {
+		result = parseInt(string_numbers);
+	}
+	return result;
 };
