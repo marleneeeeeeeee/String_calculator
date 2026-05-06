f7be4c24d3cadf416f4fef9156c2be824b38d8b8
marleneeeeeeeee <ma.haberhauer@gmail.com>
2026-05-06 11:10:54 +0200

    TC_4 created TestCase

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -3,16 +3,25 @@ describe('String Calculator', function() {
 		calculator = new StringCalculator();
 	});
 
+	//TC_1
 	it('should return 0 for an empty string', function() {
 		expect(calculator.add('')).toEqual(0);
 	});
 
+	//TC_2
 	it ('should return 1 for input "1"', function() {
 		expect(calculator.add('1')).toEqual(1);
 	});
 
+	//TC_3
 	it ('should return 3 for input "1,2"', function() {
 		expect(calculator.add('1,2')).toEqual(3);
 	});
+
+	//TC_4
+	it ('should return 28 for input "1,27"', function() {
+		expect(calculator.add('1,27')).toEqual(28);
+	});
+
 	
 });
