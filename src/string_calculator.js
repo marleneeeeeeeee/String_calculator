0db627f423b9bdfe90850f49d898d32b803095f2
marleneeeeeeeee <ma.haberhauer@gmail.com>
2026-05-06 14:21:17 +0200

    Refactoring done

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,12 +1,16 @@
-
 // StringCalculator.js
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
-    var numbers = string_numbers.split(',');
     var result = 0;
-    while (numbers.length > 0) {
-        result += parseInt(numbers[0]);
-        numbers.shift();
+    if(string_numbers.includes(',')) {
+        var numbers = string_numbers.split(',');
+        while (numbers.length > 0) {
+            result += parseInt(numbers[0]);
+            numbers.shift();
+        }
+    }
+    else {
+        result = parseInt(string_numbers) || 0;
     }
     return result;
 };
