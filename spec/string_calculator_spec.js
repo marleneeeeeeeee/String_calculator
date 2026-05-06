e22b124ddf339f10ee234863c29b68718f2e2c19
marleneeeeeeeee <ma.haberhauer@gmail.com>
2026-05-06 11:04:36 +0200

    TC_3 created Test

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -10,4 +10,8 @@ describe('String Calculator', function() {
 	it ('should return 1 for input "1"', function() {
 		expect(calculator.add('1')).toEqual(1);
 	});
+
+	it ('should return 3 for input "1,2"', function() {
+		expect(calculator.add('1,2')).toEqual(3);
+	});
 });
